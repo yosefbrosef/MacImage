09427dd7b7c9b3e4dc75d6f35d680f51c13414a7
yosefbrosef <52250266+yosefbrosef@users.noreply.github.com>
2019-06-26 09:37:19 -0700

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install tree
+
+# Install MacOs Applications
+brew cask install google-chrome
+brew cask install firefox
+brew cask install microsoft-office
+brew cask install code42-crashplan
+brew cask install zoomus
+brew cask install zoom-outlook-plugin
+brew cask install slack
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
